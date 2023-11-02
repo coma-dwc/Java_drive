897a5b011cd0ecaec3f074f85f35f260ffd84305
coma-dwc <tamagomapon@gmail.com>
2023-11-02 13:30:53 +0900

    フィールド　データの値を保管

--- src/intro_3_2/Robot.java
+++ src/intro_3_2/Robot.java
@@ -25,6 +25,7 @@ class Robot {
 //Robotクラスでは、挨拶を行うhelloメソッドとbyeメソッド、それに最初に呼び出されるmainメソッドの3つのメソッドがクラスの中で定義されています。
 //今回はHelloとByeをそれぞれ別の目的としてメソッドを分けましたが、挨拶というメソッドにまとめた場合は次のようになります
 
+/*
 class Robot {
 	public static void main(String[] args) {
 		Robot robot = new Robot();
@@ -36,6 +37,37 @@ class Robot {
 		System.out.println(msg);
 	}
 }
+*/
 
 //今回のような簡単な場合はひとつのメソッドでまとめたほうが簡単ですが、例えば、Helloと挨拶する時とByeと挨拶する時とで、
 //それぞれ異なるより複雑な処理を行うような場合には別々のメソッドにしたほうがいいかもしれません
+
+
+
+
+//■フィールドとは：クラスの中でデータの値を保管するために使用するもの
+//以下、先ほどのロボットのクラスを修正して、ロボットを動かすためのメソッドと現在位置を保管するためのフィールドを追加する
+
+class Robot {
+	int currentPosition = 0; //データ型がint フィールド名がcurrentPosition
+	
+	public static void main(String[] args) {
+		Robot robot = new Robot();
+		robot.report();
+		robot.moveForward();
+		robot.report();
+	}
+	
+	void report() {
+		System.out.println("Current Position = " + currentPosition); //reportメソッド：現在位置を画面に表示
+	}
+	
+	void moveForward() {
+		currentPosition = currentPosition + 1; //moveForwardメソッド：現在地から一つ前進する
+	}
+}
+
+//moveForwardメソッドが呼び出されると、ロボットは現在位置から一つ前進します。
+//この時現在地を保管するために使用するのがcurrentPositionフィールドです。
+//そしてreportメソッドを呼び出すとcurrentPositionフィールドに保管されている現在位置を画面に表示します。
+//このようにフィールドを用意することで、あとで使用する可能性があるデータを保管しておくことが出来ます
