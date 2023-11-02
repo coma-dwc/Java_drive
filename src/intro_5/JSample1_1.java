1f46f85c2d657b8bd5f82642bed822890192c76b
coma-dwc <tamagomapon@gmail.com>
2023-11-02 16:06:23 +0900

    文字リテラルと文字コード

---
+++ src/intro_5/JSample1_1.java
@@ -0,0 +1,17 @@
+package intro_5;
+
+//■文字リテラルの表し方:シングルクォーテーションで囲む
+//文字リテラルは1文字のリテラルなので、複数の文字が含まれている場合はエラーとなる(空の文字リテラルもエラーになる)
+class JSample1_1 {
+	public static void main(String[] args) {
+		System.out.println('a'); //a と出力
+		System.out.println('あ'); //あ と出力
+		System.out.println('\u0061'); //a と出力
+		System.out.println(0x0061); //97 と出力
+		
+		/*
+		 * 文字コードを使った文字を表す場合には、エスケープシーケンスの一つである形式で表します。
+		 * xxxx の部分に文字コードを 16 進数で指定する(今回の場合はaと出力させるために\u0061と入力した)
+		 */
+	}
+}
