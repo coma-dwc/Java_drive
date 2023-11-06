e4b83593dc4e05357a74359ffd89f43b730170a1
coma-dwc <tamagomapon@gmail.com>
2023-11-06 15:01:45 +0900

    基本のデータ型

--- src/intro_6/JSample1_1.java
+++ src/intro_6/JSample1_1.java
@@ -5,13 +5,14 @@ package intro_6;
 class JSample1_1 {
 	public static void main(String[] args) {
 		int sum = 58 + 72 + 90;
-		System.out.println("合計" + sum);
-		System.out.println("平均" + sum / 3);
+		System.out.println("合計" + sum); //出力結果：合計 220
+		System.out.println("平均" + sum / 3); //出力結果：平均 73
 	}
 }
 */
 
 
+/*
 //■変数を宣言(同じデータ型の値を格納する複数の変数をまとめて宣言)
 class JSample1_1 {
 	public static void main(String[] args) {
@@ -19,7 +20,28 @@ class JSample1_1 {
 		old = 20;
 		height = 184;
 		
-		System.out.println("年齢：" + old);
-		System.out.println("身長：" + height);
+		System.out.println("年齢：" + old); //出力結果：年齢：20
+		System.out.println("身長：" + height); //出力結果：身長：184
+	}
+}
+*/
+
+
+//■データ型
+class JSample1_1 {
+	public static void main(String[] args) {
+		int n = 100; //整数のデータ型(基本 int型)
+		long l = 12345678910L; //整数のデータ型(long型 きわめて大きな数値)
+		double d = 3.14; //浮動小数点数のデータ型(double型 64ビット倍精度浮動小数点数)
+		float f = 3.14F; //浮動小数点数のデータ型(float型 32ビット単精度浮動小数点数)
+		char c = '花'; //文字のデータ型(一文字)
+		boolean b = true; //論理型のデータ型
+		
+		System.out.println("n=" + n); //出力結果：n=100
+		System.out.println("l=" + l); //出力結果：l=12345678910
+		System.out.println("d=" + d); //出力結果：d=3.14
+		System.out.println("f=" + f); //出力結果：f=3.14
+		System.out.println("c=" + c); //出力結果：c=花
+		System.out.println("b=" + b); //出力結果：b=true
 	}
 }
