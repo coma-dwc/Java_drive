021fcf300c4cefd0897cb8fceab1a1c4f06fd84e
coma-dwc <tamagomapon@gmail.com>
2023-11-06 14:40:45 +0900

    変数の宣言

--- src/intro_6/JSample1_1.java
+++ src/intro_6/JSample1_1.java
@@ -1,9 +1,25 @@
 package intro_6;
 
+/*
+//■Javaにおける変数とは
 class JSample1_1 {
 	public static void main(String[] args) {
 		int sum = 58 + 72 + 90;
 		System.out.println("合計" + sum);
 		System.out.println("平均" + sum / 3);
+	}
+}
+*/
+
+
+//■変数を宣言(同じデータ型の値を格納する複数の変数をまとめて宣言)
+class JSample1_1 {
+	public static void main(String[] args) {
+		int old, height;
+		old = 20;
+		height = 184;
+		
+		System.out.println("年齢：" + old);
+		System.out.println("身長：" + height);
 	}
 }
