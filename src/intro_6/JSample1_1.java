d5cfa541bf48b5b75045aeef5c36fd4b3b9f0dde
coma-dwc <tamagomapon@gmail.com>
2023-11-06 14:34:38 +0900

    Javaにおける変数

---
+++ src/intro_6/JSample1_1.java
@@ -0,0 +1,9 @@
+package intro_6;
+
+class JSample1_1 {
+	public static void main(String[] args) {
+		int sum = 58 + 72 + 90;
+		System.out.println("合計" + sum);
+		System.out.println("平均" + sum / 3);
+	}
+}
