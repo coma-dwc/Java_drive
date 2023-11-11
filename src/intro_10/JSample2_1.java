6219e4bbfb70d5fd29a88ba8a0c1c4c54cb39474
coma-dwc <tamagomapon@gmail.com>
2023-11-11 23:14:46 +0900

    配列の要素を逆順に並び替える

--- src/intro_10/JSample2_1.java
+++ src/intro_10/JSample2_1.java
@@ -337,7 +337,7 @@ true
 
 
 //■Arrays.deepEqualsメソッドを使って比較する
-
+/*
 import java.util.Arrays;
 
 class JSample2_1 {
@@ -362,4 +362,30 @@ false
 true
 true
 true
+*/
+
+
+
+//■配列の要素を逆順に並び替える
+
+import java.util.Arrays;
+
+class JSample2_1 {
+	public static void main(String[] args) {
+		int[] src = {12, 24, 18, 31, 17};
+		System.out.println(Arrays.toString(src));
+		
+		for(int f = 0, l = src.length - 1; f < l; f++, l--) {
+			int temp = src[f];
+			src[f] = src[l];
+			src[l] = temp;
+		}
+		
+		System.out.println(Arrays.toString(src));
+	}
+}
+/*
+出力結果：
+[12, 24, 18, 31, 17]
+[17, 31, 18, 24, 12]
 */
