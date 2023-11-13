0ddc9f7f8d85437429e700edf79e484c5b78661e
coma-dwc <tamagomapon@gmail.com>
2023-11-14 01:17:18 +0900

    メソッド

---
+++ src/intro_12/JSample1_1.java
@@ -0,0 +1,31 @@
+package intro_12;
+
+//■メソッド
+//checkという名のメソッドを用意
+
+class JSample1_1 {
+	public static void main(String[] args) {
+		int eigo = 78;
+		int suugaku = 90;
+		int kokugo = 68;
+		
+		check("英語", eigo);
+		check("数学", suugaku);
+		check("国語", kokugo);
+	}
+	
+	private static void check(String kyoka, int seiseki) {
+		System.out.print(kyoka + "の試験結果は");
+		if(seiseki > 80) {
+			System.out.println("合格です");
+		} else {
+			System.out.println("不合格です");
+		}
+	}
+}
+/*
+出力結果：
+英語の試験結果は不合格です
+数学の試験結果は合格です
+国語の試験結果は不合格です
+*/
