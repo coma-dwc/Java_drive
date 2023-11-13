db7f73bd95f477bf60a95fe96a2b8c1127525fbd
coma-dwc <tamagomapon@gmail.com>
2023-11-14 03:46:12 +0900

    try文

--- src/intro_14/JSample1_1.java
+++ src/intro_14/JSample1_1.java
@@ -21,7 +21,7 @@ class JSample1_1 {
 
 
 //実行時のエラー
-
+/*
 class JSample1_1 {
 	public static void main(String args[]) {
 		int n[] = {18, 29, 36};
@@ -49,3 +49,40 @@ Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out
 //Javaではこのようにプログラムの実行中にエラーが発生することを「例外」が発生したと言う
 
 //「例外」が発生した時に単にプログラムを終了させるのではなく指定した動作をさせるための仕組みが用意されている。それが「例外処理」と呼ばれるもの
+
+
+
+
+//■try文
+//例外処理を行うにはtry文を使用する
+
+class JSample1_1 {
+	public static void main(String args[]) {
+		int n[] = {18, 29, 36};
+		
+		System.out.println("開始します");
+		
+		try {
+			for(int i = 0; i < 4; i++) {
+				System.out.println(n[i]);
+			}
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("配列の範囲を超えています");
+		}
+		
+		System.out.println("終了しました");
+	}
+}
+/*
+出力結果：
+開始します
+18
+29
+36
+配列の範囲を超えています
+終了しました
+*/
+
+//例外が発生してもエラーメッセージは表示されずcatchブロック内に記述した処理が実行されている。
+//そしてプログラムも途中で終了することなくtry文以降の分も実行されている
