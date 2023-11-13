6a2fb28def15f98e09576b74a409d73743e4d662
coma-dwc <tamagomapon@gmail.com>
2023-11-14 03:54:13 +0900

    必ず実行する処理の記述(try..catch..finally)

--- src/intro_14/JSample1_1.java
+++ src/intro_14/JSample1_1.java
@@ -55,7 +55,7 @@ Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out
 
 //■try文
 //例外処理を行うにはtry文を使用する
-
+/*
 class JSample1_1 {
 	public static void main(String args[]) {
 		int n[] = {18, 29, 36};
@@ -86,3 +86,41 @@ class JSample1_1 {
 
 //例外が発生してもエラーメッセージは表示されずcatchブロック内に記述した処理が実行されている。
 //そしてプログラムも途中で終了することなくtry文以降の分も実行されている
+
+
+
+//■必ず実行する処理の記述(try..catch..finally)
+//finallyブロックに記述した処理は例外が発生してもしなくても必ず実行される。
+//また、catchブロック内でreturn文が実行されて呼び出し元へ処理が移る場合でもfinallyブロック内の処理は実行されている
+
+class JSample1_1 {
+	public static void main(String args[]) {
+		disp(1);
+		disp(2);
+		disp(3);
+	}
+	
+	private static void disp(int no) {
+		int n[] = {18, 29, 36};
+		
+		try {
+			System.out.println(n[no]);
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("配列の範囲を超えています");
+			return;
+		}
+		finally {
+			System.out.println("要素の出力を終了します");
+		}
+	}
+}
+/*
+出力結果：
+29
+要素の出力を終了します
+36
+要素の出力を終了します
+配列の範囲を超えています
+要素の出力を終了します
+*/
