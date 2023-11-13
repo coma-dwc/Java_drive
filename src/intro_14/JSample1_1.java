5d6e0cfabb6ca403abcb57599518e24c613b1b24
coma-dwc <tamagomapon@gmail.com>
2023-11-14 03:34:58 +0900

    例外とは

---
+++ src/intro_14/JSample1_1.java
@@ -0,0 +1,51 @@
+package intro_14;
+
+//■例外処理
+
+//■例外とは
+/*
+class JSample1_1 {
+	public static void main(String args[]) {
+		int n = 10;
+		
+		if(n == 10{    // )がないのでコンパイルエラーが発生する
+			System.out.println("10です");
+		}
+	}
+}
+/*
+出力結果：
+コンパイルエラー
+*/
+
+
+
+//実行時のエラー
+
+class JSample1_1 {
+	public static void main(String args[]) {
+		int n[] = {18, 29, 36};
+		
+		System.out.println("開始します");
+		
+		for(int i = 0; i < 4; i++) {
+			System.out.println(n[i]);
+		}
+		
+		System.out.println("終了しました");
+	}
+}
+/*
+出力結果：
+開始します
+18
+29
+36
+Exception in thread "main" java.lang.ArrayIndexOutOfBoundsException: Index 3 out of bounds for length 3
+	at intro_14.JSample1_1.main(JSample1_1.java:32)
+*/
+
+//途中でエラーが発生 → 配列の要素が3つしかないのに4つ目の要素を参照しようとしたため
+//Javaではこのようにプログラムの実行中にエラーが発生することを「例外」が発生したと言う
+
+//「例外」が発生した時に単にプログラムを終了させるのではなく指定した動作をさせるための仕組みが用意されている。それが「例外処理」と呼ばれるもの
