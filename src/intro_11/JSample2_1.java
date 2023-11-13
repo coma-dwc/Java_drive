27659039d063186f446b4c0c6f1abf66d0794570
coma-dwc <tamagomapon@gmail.com>
2023-11-13 15:07:22 +0900

    数値を文字列に変換

--- src/intro_11/JSample2_1.java
+++ src/intro_11/JSample2_1.java
@@ -26,7 +26,7 @@ Hello
 
 
 //String型の変数に別の文字列を代入
-
+/*
 class JSample2_1 {
 	public static void main(String[] args) {
 		String msg1 = "Hello";
@@ -50,4 +50,100 @@ Hello
 ----
 Hello
 Bye!
+*/
+
+
+
+//■数値を文字列に変換
+
+//ラッパークラスのtoStringメソッドを使用
+/*
+class JSample2_1 {
+	public static void main(String[] args) {
+		int i = 10;
+		long l = 12L;
+		double d = 7.82;
+		boolean b = true;
+		
+		String si = Integer.valueOf(i).toString();
+		String sl = Long.valueOf(l).toString();
+		String sd = Double.valueOf(d).toString();
+		String sb = Boolean.valueOf(b).toString();
+		
+		System.out.println(si);
+		System.out.println(sl);
+		System.out.println(sd);
+		System.out.println(sb);
+	}
+}
+/*
+出力結果：
+10
+12
+7.82
+true
+*/
+
+
+
+//StringクラスのvalueOfメソッドを使用
+/*
+class JSample2_1 {
+	public static void main(String[] args) {
+		int i = 10;
+		short s = 21;
+		long l = 12L;
+		double d = 7.82;
+		boolean b = true;
+		
+		String si = String.valueOf(i);
+		String ss = String.valueOf(s);
+		String sl = String.valueOf(l);
+		String sd = String.valueOf(d);
+		String sb = String.valueOf(b);
+		
+		System.out.println(si);
+		System.out.println(ss);
+		System.out.println(sl);
+		System.out.println(sd);
+		System.out.println(sb);
+	}
+}
+/*
+出力結果：
+10
+21
+12
+7.82
+true
+*/
+
+
+
+//+演算子を使って文字列と連結
+
+class JSample2_1 {
+	public static void main(String[] args) {
+		int i = 10;
+		long l = 12L;
+		double d = 7.82;
+		boolean b = true;
+		
+		String si = "" + i;
+		String sl = "" + l;
+		String sd = "" + d;
+		String sb = "" + b;
+		
+		System.out.println(si);
+		System.out.println(sl);
+		System.out.println(sd);
+		System.out.println(sb);
+	}
+}
+/*
+出力結果：
+10
+12
+7.82
+true
 */
